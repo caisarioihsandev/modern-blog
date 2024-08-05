e08b3f6e9795551cfe507a6c702d62527904d828
caisarioihsandev <60533597+caisarioihsandev@users.noreply.github.com>
2024-08-05 15:05:47 +0700

    Update db.js
    for temporary set manually database parameter

--- config/db.js
+++ config/db.js
@@ -4,10 +4,10 @@ const dotenv = require("dotenv");
 dotenv.config({ path: './.env' })
 
 const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
+    host: process.env.DATABASE_HOST || "localhost",
+    user: process.env.DATABASE_USERNAME || "u2934543_viyori",
+    password: process.env.DATABASE_PASSWORD || "admin1234",
+    database: process.env.DATABASE || "u2934543_blogging"
 });
 
 // MYSQL connection
