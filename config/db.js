a87cab9f0d72bcee3b907e99386debd3baa45ef9
caisarioihsandev <60533597+caisarioihsandev@users.noreply.github.com>
2024-08-07 22:48:05 +0700

    Update db.js change port 3306
    Changing port 3306

--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@ const dotenv = require("dotenv");
 dotenv.config({ path: './.env' })
 
 const db = mysql.createConnection({
-    host: "153.92.8.43",
-    port: 995,
+    host: "localhost",
+    port: 3306,
     user: "u2934543_viyori",
     password: "admin1234",
     database: "u2934543_blogging"
